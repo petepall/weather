94b2166e3593cb01c546919d253917cd1d0d0a4e
Peter Pallen <peter@ppallen.be>
2022-11-21 23:01:44 +0100

    ✨ added alternate data source

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,29 @@
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
-import { Weather } from './ICurrentWeather.js';
-import { weatherCodesArray } from './weatherCodes.js';
+import { IWeatherAPI } from './IWeatherApi.js';
 
 const request: AxiosInstance = axios.create({
-	baseURL: 'https://api.open-meteo.com',
+	// baseURL: 'https://api.open-meteo.com',
+	baseURL: 'https://fcc-weather-api.glitch.me',
 	timeout: 1000,
 });
 
 const response: AxiosResponse = await request.get(
-	'v1/forecast?latitude=50.82&longitude=5.19&current_weather=true',
+	// 'v1/forecast?latitude=50.82&longitude=5.19&current_weather=true',
+	'/api/current?lat=50.82&lon=5.19',
 );
 
-const weather: Weather = response.data;
-const weatherCode: { [key: string]: string } | undefined =
-	weatherCodesArray.find((code) => code[weather.current_weather.weathercode]);
+const weather: IWeatherAPI = response.data;
 
-if (weatherCode) {
-	weather.current_weather.weathercode += ` - ${
-		weatherCode[weather.current_weather.weathercode]
-	}`;
-} else {
-	console.log('Weather code not found');
-}
+// const weather: Weather = response.data;
+// const weatherCode: { [key: string]: string } | undefined =
+// 	weatherCodesArray.find((code) => code[weather.current_weather.weathercode]);
+
+// if (weatherCode) {
+// 	weather.current_weather.weathercode += ` - ${
+// 		weatherCode[weather.current_weather.weathercode]
+// 	}`;
+// } else {
+// 	console.log('Weather code not found');
+// }
 
 console.log(weather);
